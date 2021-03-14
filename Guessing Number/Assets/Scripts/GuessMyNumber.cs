a609ab1b402ffb04f76a76e1be2dce8739c24458
onurinal <onur.inall01@gmail.com>
2021-03-14 17:02:17 +0300

    Basic Project

---
+++ Guessing Number/Assets/Scripts/GuessMyNumber.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GuessMyNumber : MonoBehaviour
+{
+    [SerializeField] private int _max;
+    [SerializeField] private int _min;
+    [SerializeField] TextMeshProUGUI guessText;
+    private int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        GuessNumber();
+    }
+    private void GuessNumber()
+    {
+
+        
+        guess = Random.Range(_min, _max + 1);
+        guessText.text = guess.ToString();
+    }
+    public void onPressHigher()
+    {
+        if (_max == _min)
+        {
+            GuessNumber();
+        }
+        else
+        {
+            _min = guess + 1;
+            GuessNumber();
+        }
+    }
+    public void onPressLower()
+    {
+        if(_max == _min)
+        {
+            GuessNumber();
+        }
+        else
+        {
+            _max = guess - 1;
+            GuessNumber();
+        }
+    }
+}
---
+++ Guessing Number/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currenScenetIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currenScenetIndex + 1);
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadQuit()
+    {
+        Application.Quit();
+    }
+}
