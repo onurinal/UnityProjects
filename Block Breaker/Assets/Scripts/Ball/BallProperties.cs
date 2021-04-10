b040a84940bd49591a7e0ef9c88ff0bdaa3440f0
onurinal <onur.inall01@gmail.com>
2021-04-10 20:17:18 +0300

    + Added some new properties for ball + Refactor

--- Block Breaker/Assets/Scripts/Ball/BallProperties.cs
+++ Block Breaker/Assets/Scripts/Ball/BallProperties.cs
@@ -8,8 +8,13 @@ namespace BlockBreaker.Ball
     public class BallProperties : ScriptableObject
     {
         [Header("Ball Movement")]
-        public float BallXSpeed = 0f;
-        public float BallYSpeed = 7f;
+        // ------- Ball Start Speed -------
+        public float BallXSpeed = 0f;       // for starting
+        public float BallYSpeed = 7f;       // for starting
+        // ------- Ball Base Speed -------  // make the ball constant speed.
+        public float BallBaseSpeed = 8f;
+        [Space(20)]
+        public float BallPushX = 200f;  // for paddle collision. If ball hits left side it will force left movement in PaddleController.cs
     }
 }
 
