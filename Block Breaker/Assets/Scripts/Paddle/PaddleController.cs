1a006d98a27235a447681e75c63a854bda421552
onurinal <onur.inall01@gmail.com>
2021-04-10 20:23:23 +0300

    + Added AddForce to the ball to prevent infinite loop vertically

--- Block Breaker/Assets/Scripts/Paddle/PaddleController.cs
+++ Block Breaker/Assets/Scripts/Paddle/PaddleController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using BlockBreaker.LevelSystem;
 using BlockBreaker.Ball;
 
 namespace BlockBreaker.Paddle
@@ -10,10 +9,17 @@ namespace BlockBreaker.Paddle
     {
         [Header("Paddle Properties")]
         [SerializeField] private PaddleProperties _playerProperties = null;
+        [SerializeField] private BallProperties _ballProperties = null;
 
         private SpriteRenderer _paddle;
         private float _minXAndroid = 0f, _maxXAndroid = 0f;
         private float _paddleX = 0f;  // paddle x size to fix screen boundaries
+
+        public static PaddleController Instance;
+        private void Awake()
+        {
+            Instance = this;
+        }
         private void Start()
         {
             SetUpMovementBoundaries();
@@ -22,7 +28,7 @@ namespace BlockBreaker.Paddle
         {
             TouchMovement();
         }
-
+        // ----------------------- PADDLE MOVEMENT AND SET UP BOUNDARY ---------------
         private void TouchMovement()
         {
             Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -39,6 +45,26 @@ namespace BlockBreaker.Paddle
             Camera gameCamera = Camera.main;
             _minXAndroid = gameCamera.ScreenToWorldPoint(new Vector3(0, 0, 0)).x + _paddleX;  
             _maxXAndroid = gameCamera.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x - _paddleX;
+        }
+        // ----------------------- BALL CONTROLLING ---------------
+        // If the ball hits left side on paddle we will add force for ball to move left. It is the same for right side too
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Rigidbody2D ballRigidBody = collision.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 hitPoint = collision.contacts[0].point;
+            float difference = transform.position.x - hitPoint.x;
+            if (hitPoint.x < transform.position.x)
+            {
+                ballRigidBody.AddForce(new Vector2(-(Mathf.Abs(_ballProperties.BallPushX)), 0));
+            }
+            else
+            {
+                ballRigidBody.AddForce(new Vector2(Mathf.Abs(_ballProperties.BallPushX), 0));
+            }
+        }
+        public void ResetPaddlePosition()
+        {
+            transform.position = new Vector3(Camera.main.transform.position.x, transform.position.y, 0);
         }
     }
 }
