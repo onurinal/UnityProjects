cbc1c0e26b8d01597d9b23af1a6bf3ad31e7cccd
onurinal <onur.inall01@gmail.com>
2021-04-10 20:12:03 +0300

    + Resize background and repositioning the colliders automatically for every aspect ratio

---
+++ Block Breaker/Assets/Scripts/MakeResize.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeResize : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _gameplayBackground = null;
+    [SerializeField] private SpriteRenderer _topCollider = null;
+    [SerializeField] private Transform _rightCollider = null;
+    [SerializeField] private Transform _leftCollider = null;
+    private float _worldScreenWidth;
+    private float _worldScreenHeight;
+    private void Awake()
+    {
+        // ----------------------- RESIZE GAMEPLAY BACKGROUND TO ASPECT RATIO ---------------
+        _worldScreenHeight = Camera.main.orthographicSize * 2f;
+        _worldScreenWidth = _worldScreenHeight / Screen.height * Screen.width;
+
+        // _gameplayBackground.sprite.bounds.size.x,  GET THE SPRITE SIZES X AND Y
+        // _gameplayBackground.sprite.bounds.size.y,
+
+        _gameplayBackground.transform.localScale = new Vector3
+            (_worldScreenWidth / _gameplayBackground.sprite.bounds.size.x,
+             _worldScreenHeight / _gameplayBackground.sprite.bounds.size.y,
+             _gameplayBackground.transform.localScale.z);
+        // ----------------------- RESIZE GAMEPLAY BACKGROUND TO ASPECT RATIO ---------------
+        _topCollider.transform.localScale = new Vector3(_worldScreenWidth / _topCollider.sprite.bounds.size.x,
+                                                        _topCollider.transform.localScale.y,
+                                                        _topCollider.transform.localScale.z);
+        // ----------------------- REPOSITIONING COLLIDER TO ASPECT RATIO ---------------
+        _rightCollider.transform.position = new Vector3(_worldScreenWidth / 2 + 0.5f,
+                                                        _rightCollider.transform.position.y,
+                                                        _rightCollider.transform.position.z);
+        _leftCollider.transform.position = new Vector3(-(_worldScreenWidth / 2 + 0.5f),
+                                                       _leftCollider.transform.position.y,
+                                                       _leftCollider.transform.position.z);
+    }
+}
